040bc9ffc1d67cd9627501f5a7099a172960c029
co.arce <co.arce@ibm.com>
2026-06-30 17:00:35 -0400

    cambio role

--- prisma/add-important-users.js
+++ prisma/add-important-users.js
@@ -48,8 +48,8 @@ async function main() {
     email: 'josu.campusano@duocuc.cl',
     username: 'jo.campusano',
     name: 'Josue Campusano',
-    role: 'VIEWER',
-    password: 'viewer123',
+    role: 'STOCK',
+    password: 'stock123',
   });
 
   await upsertUser({
